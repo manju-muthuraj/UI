2ea2e1ed9268260785d3d7f098b450ae19ba5127
Manjunatha M <63409760+manju-muthuraj@users.noreply.github.com>
2020-05-10 19:14:13 +0530

    boxes and customers

---
+++ CSS/boxes/shared.js
@@ -0,0 +1,66 @@
+let backdrop = document.querySelector('.backdrop');
+let selectPlanButtons = document.querySelectorAll('.plan button');
+let modal = document.querySelector('.modal');
+let modalNoButton = document.querySelector('.modal__action--negative');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
+let ctaButton = document.querySelector('.main-nav__item--cta');
+//console.dir(backdrop.style['background-image']);
+//console.dir(backdrop.style.backgroundImage);
+
+for(let i= 0; i < selectPlanButtons.length; i++){
+    selectPlanButtons[i].addEventListener('click', function () {
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open'; // This will actually overwrite the complete class list
+        modal.classList.add('open'); // This will attach the class to modal class
+        backdrop.style.display = 'block';
+        setTimeout(function () {
+            backdrop.classList.add('open');
+        }, 10);
+    });
+}
+
+backdrop.addEventListener('click', function () {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+if(modalNoButton){
+    modalNoButton.addEventListener('click', closeModal);
+}
+
+function closeModal() {
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    if(modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+    setTimeout(function () {
+        backdrop.style.display = 'none';
+    }, 200);
+}
+
+toggleButton.addEventListener('click',function () {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(function () {
+    backdrop.classList.add('open');
+    }, 10);
+});
+
+ctaButton.addEventListener('animationstart', function(event) {
+    console.log('Animation Started', event);
+});
+
+ctaButton.addEventListener('animationend', function(event) {
+    console.log('Animation ended', event);
+});
+
+ctaButton.addEventListener('animationiteration', function(event) {
+    console.log('Animation iteration', event);
+});
